a3c7f6b172b26e8378567508bf56c90affb67b85
Kevin Le <iamkevinvle@gmail.com>
2024-09-04 16:38:41 -0700

    modify base path of navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 function loadNavbar() {
-  fetch('/d277/components/navbar.html')
+  fetch('/components/navbar.html')
     .then((response) => response.text())
     .then((data) => {
       document.getElementById('navbar').innerHTML = data;
